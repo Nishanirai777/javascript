bb91686c414ff09ba634b96ecb26d23a234c1b4b
Nisha <rainishani400@gmail.com>
2024-09-30 19:27:32 +0545

    Add files via upload

---
+++ if.js
@@ -0,0 +1,43 @@
+
+console.log("after my if statements");
+//ps
+//traffic light system
+let color = "green";
+if (color === "red") {
+    console.log("stop! light color is red");
+
+
+}
+else if  (color === "yellow") {
+    console.log("slow down light color si yellow");
+}
+ else if (color === "green") {
+    console.log("go.. Light color is green");
+}
+let age = 28;
+if (age >= 18) {
+    console.log("you can vote");
+}
+else if (age < 18) {
+    console.log("you can not vote");
+
+}
+else{
+    console.log("Traffic light is broken");
+}
+//console.log("before my if statements");
+//let age = 2;
+//if (age >= 18) {
+//  console.log("you can vote");
+//}
+let marks = 56;
+if (marks >= 80) {
+    console.log("A++");
+
+}
+else if (marks >= 75) {
+    console.log("A");
+}
+else if (marks < 30) {
+    console.log("F");
+}
