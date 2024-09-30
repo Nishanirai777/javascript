418f4cb74e5be749eb79aa779d31199842acc2b6
Nisha <rainishani400@gmail.com>
2024-09-30 18:30:40 +0545

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,42 @@
+
+console.log("hello");
+//Arithmetic operators
+let a=90;
+let b=90;
+console.log(a-b);
+console.log(a+b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+//unary operators
+let c=9;
+console.log(c++)//9
+console.log(++c);//11
+//assignment operators (=,+=,*=,%=etc)
+let s=8;
+let r=7;
+s-=r;
+console.log(s);
+//comparison operators
+//== compares value ,not a type 
+//=== compare both vale and type
+age=18;
+console.log(age>16);
+//logical operators
+let u=9;
+let v=8;
+console.log((u>v)&& (v>u)); 
+//in case of logical anadd && if both are ture then true otherwwise not 
+//if one false both false 
+//if one true bothe false
+let marks =99;
+if(marks>=90 && marks>=99){
+console.log("pass");
+console.log("A++");
+}
+//for or operators 
+//true|| true=true
+//true||false=true
+//false||true=true
+//false||false=false
+//! logicalNot which gives true to false and false to true
