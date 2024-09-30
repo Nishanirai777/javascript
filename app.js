f74e0df25968f7277e4ad11e0e7bc95f5686395e
Nisha <rainishani400@gmail.com>
2024-09-30 16:55:46 +0545

    Add files via upload
    java

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("hello javaScript");
+let num1=23;
+let num2=90;
+console.log("the sum is:" ,num1+num2);
+let a=34;
+let b=89;
+let c=  "The sub is:"+(a-b); 
+
+console.log(c);
+//``it is called back ticks
+let pen=50;
+let copy=78;
+console.log(`The total is:${pen + copy} rupees.`);
