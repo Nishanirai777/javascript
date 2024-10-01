85dfb925f5fbee239dd196625c2b9f31e2b3a1d4
Nisha <rainishani400@gmail.com>
2024-10-01 16:56:13 +0545

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,14 @@
+let color = "red";
+switch(color){
+    case "red":
+        console.log("stop all");
+        break;
+        case "yellow":
+            console.log("slow down");
+            break;
+            case "green":
+                console.log("go");
+                break;
+                default:
+                    console.log("light is broken");
+}
