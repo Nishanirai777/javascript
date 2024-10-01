02245938bf41812a3e9177fd2cd5c32b317c49ca
Nisha <rainishani400@gmail.com>
2024-10-01 17:29:14 +0545

    Add files via upload

---
+++ alerts.js
@@ -0,0 +1,12 @@
+alert("danger");
+console.log("this is a simple message");
+console.error("this is an error message");
+console.warn("warning for you");
+//prompt
+ let name = prompt("enter your name...");
+ console.log(name);
+ let firstName = prompt("enter the first name..");
+ let lastName =prompt("enter your last name...");
+ //console.log("welcome" ,firstName ,lastName,"!");
+ let msg = "welcome" +firstName + " "+  lastName + "!";
+ alert(msg);
