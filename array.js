fc4c0fbd68286413de8871e2c72f7a231b8f7798
Nisha <rainishani400@gmail.com>
2024-10-02 18:39:15 +0545

    Add files via upload
    array

---
+++ array.js
@@ -0,0 +1,10 @@
+let students= ["saraj", "nisha","lalita"];
+let name= ["nisha",90, 1];
+//name[0]="kali"; for the replace of name
+let cars =["audi" ,"bmw","xuv","maruti"];
+cars.push("abc");//for add new car
+cars.pop("audi");//for remove the cars
+cars.unshift("fff");//for add a new car
+cars.shift();//it delete the fff
+//ps
+let months =["january", "july", "march", "auguest"];
