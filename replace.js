08e9ded2d5eb7a6f2388aed1bdda6d70f0ca47ac
Nisha <rainishani400@gmail.com>
2024-10-02 17:46:23 +0545

    Update replace.js

--- replace.js
+++ replace.js
@@ -3,3 +3,9 @@ let msg ="unitedcolllege";
  //for repeat
  let fruit = "mango";
  fruit.repeat(4);
+//ps
+let msg= "help!";
+let newmsg = msg.trim().toUpperCase();
+console.log(newmsg);
+let name = "nishanirai";
+console.log((name.slice(4).replace("r","t")));
