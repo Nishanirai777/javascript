1307428ed45b8aed0184b71303e1091d92fffa14
Nisha <rainishani400@gmail.com>
2024-10-02 15:21:32 +0545

    Update ps.js

--- ps.js
+++ ps.js
@@ -27,3 +27,9 @@ switch(quarter){
                     default:
                         console.log("wrong");
 }
+let str = "apples";
+if((str[0]== "a" || str[0]=="A") && str.length>5){
+    console.log("Golden String..");
+}else{
+    console.log("not a golden strings..");
+}
