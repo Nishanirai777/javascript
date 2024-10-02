d371921518f71a34e6e001cf5f3408a12e75e6ac
Nisha <rainishani400@gmail.com>
2024-10-02 17:09:28 +0545

    Add files via upload

---
+++ chainning.js
@@ -0,0 +1,9 @@
+let msg = "   hello    hi   "
+//this is called chaining
+//let newmsg = msg.trim().toLowerCase();
+//console.log(newmsg);
+console.log(msg);
+let newmsg = msg.trim();
+console.log(newmsg);
+newmsg= newmsg.toUpperCase();
+console.log(newmsg);
