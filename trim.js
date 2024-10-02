e09448293fafb4ab83a1163f5df286fafa40cb1e
Nisha <rainishani400@gmail.com>
2024-10-02 16:46:37 +0545

    Update trim.js

--- trim.js
+++ trim.js
@@ -3,3 +3,6 @@ str.trim();
  let pass= prompt("enter your password..");
  console.log(pass.trim() );
  //trim is used for only clear the space in first and last part not in middle
+let name= "Nishani Kumari rai";
+//name.toLowerCase();
+//name.toUpperCase();
