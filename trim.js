ecb39a809d02d501bbf350be597b063a849ff8f1
Nisha <rainishani400@gmail.com>
2024-10-02 16:38:28 +0545

    Add files via upload

---
+++ trim.js
@@ -0,0 +1,5 @@
+let str = "   hello  ";
+str.trim();
+ let pass= prompt("enter your password..");
+ console.log(pass.trim() );
+ //trim is used for only clear the space in first and last part not in middle
