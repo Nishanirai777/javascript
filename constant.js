48003ce69d49c7c3d2a350adde2bde812f8e499f
Nisha <rainishani400@gmail.com>
2024-10-03 13:44:00 +0545

    Update constant.js

--- constant.js
+++ constant.js
@@ -3,3 +3,8 @@ console.log(arry);
 //in arry we can cange the value 
 arry.push("8");//for add a new one
 arry.pop();
+//nested array
+let num =[ [1,2],[2,3],[4,5]];
+num[0];
+num[0].length;
+num[1][1]//to print the value
