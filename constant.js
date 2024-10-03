928667b38b7a2d5b70fd9ef066a20b19d854ef92
Nisha <rainishani400@gmail.com>
2024-10-03 14:00:08 +0545

    Update constant.js

--- constant.js
+++ constant.js
@@ -8,3 +8,7 @@ let num =[ [1,2],[2,3],[4,5]];
 num[0];
 num[0].length;
 num[1][1]//to print the value
+//practice questions
+let game =[['x',null,'0'],[null, 'x', '0'],['0',null,'x']];
+game[0][1]//for see the value
+game[0][1]='0'//for remove the null value and add the 0 to null
