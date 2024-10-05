fc604f08791dcfd0abc332313d7d27de8879be4a
Nisha <rainishani400@gmail.com>
2024-10-05 18:09:07 +0545

    Update forloop.js

--- forloop.js
+++ forloop.js
@@ -10,3 +10,11 @@ for(let a=1;a<=15;a=a+2){
 for(let b=1;b>=15;b=b-2){
     console.log(b);
 }
+//print even number
+for(let c=2;c<=16;c=c+2){
+    console.log(c);
+}
+//backward
+for(let d=20;d>=2;d=d-2){
+    console.log(d);
+}
