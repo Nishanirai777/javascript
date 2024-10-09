f51180bd306659c9be67f23ff547aa42d585858d
Nisha <rainishani400@gmail.com>
2024-10-09 12:04:12 +0545

    Add files via upload

---
+++ object.js
@@ -0,0 +1,23 @@
+let student = {
+    name: "Nisha Rai",
+    level: "bachelor",
+    age: 21,
+    marks: 90
+
+};
+//creating the post
+const post={
+    username:"@nisharai",
+    content:"this is my #firstpsot",
+    likes: 2000,
+    reposts:67,
+    tags:["@apnacollege","@delta"]
+};
+//to get the value we use this 
+//post["content"]
+//post["username"]
+//student.name
+//to update the  value
+// post.reposts=80;
+//post.likes=3000;
+post.content="this is my #secondpost";
---
+++ objectofobject.js
@@ -0,0 +1,38 @@
+const information= {
+nisha:{
+    name:"nishani",
+    age:21,
+    marks:80
+
+},
+saraj:{
+    name:"saraj",
+    age:22,
+    marks:100
+
+},
+unknow:{
+name:"abinas",
+age:12,
+}
+};
+//array of object 
+const informationn= [
+    nisha:{
+        name:"nishani",
+        age:21,
+        marks:80
+    
+    },
+    saraj:{
+        name:"saraj",
+        age:22,
+        marks:100
+    
+    },
+    unknow:{
+    name:"abinas",
+    age:12,
+    }
+];
+//informationn[1].saraj
