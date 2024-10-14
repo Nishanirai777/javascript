6b9d5993f113c59b9d34a56e8cd4105175ee014e
Nisha <rainishani400@gmail.com>
2024-10-14 09:59:52 +0545

    Add files via upload

---
+++ block.js
@@ -0,0 +1,19 @@
+//{}block doesnot work if you write var
+{
+    var a = 99;
+}
+console.log(a);
+{
+    let b = 90;
+    console.log(b);//{}if you write under the this bracket this is the block scope
+}
+//
+for (let i=1; i<=6; i++){
+    console.log(i); 
+}
+console.log(i);
+let age =56;
+if(age>=18){
+    let str= "adult";
+    console.log(str);
+}
