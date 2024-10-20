57369a91a08b7135db545fd313c51971ef656f4b
Nisha <rainishani400@gmail.com>
2024-10-20 16:27:30 +0545

    Update method.js

--- method.js
+++ method.js
@@ -18,3 +18,16 @@ sub(a,b){
     return a-b;
 },
 };
+//ps
+//write a javaScript function that returns array elements larger than a number
+let arr =[1,2,3,4,5,6,7,8,9,10];
+let num =5;
+function getElement(arr,num){
+    for(let i= 0; i<arr.length; i++){
+        if(arr[i]>num){
+            console.log(arr[i]);
+        }
+    }
+
+}
+getElement(arr,num);
