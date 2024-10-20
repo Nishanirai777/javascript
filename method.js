d4a4274f7995bee891d4d080a499ef50901b35ce
Nisha <rainishani400@gmail.com>
2024-10-20 16:16:08 +0545

    Add files via upload

---
+++ method.js
@@ -0,0 +1,20 @@
+const calculator={
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    },
+    mul:function(a,b){
+        return a*b;
+    }
+};
+//methods (shorthand)
+const calculatorr={
+add(a,b){
+    return a+b;
+},
+sub(a,b){
+    return a-b;
+},
+};
