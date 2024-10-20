1d5c2952038d9aef6d108fe125ded98488a17d43
Nisha <rainishani400@gmail.com>
2024-10-20 16:03:20 +0545

    Update higherorder.js

--- higherorder.js
+++ higherorder.js
@@ -12,3 +12,30 @@ let greet= function(){
     
 }
 multipleGreet(greet,1000);
+//higher oder 
+let odd = function (n){
+    console.log(!(n%2==2));
+} 
+//for even 
+let even = function(n)
+
+{
+    console.log(n%2==0);
+}
+//oddeventest
+function oddEvenFactory(request){
+if(request=="odd")
+{
+    let odd = function (n){
+        console.log(!(n%2==2));
+    } 
+    return odd;
+} else if (request=="even"){
+        let even = function(n){
+            console.log(n%2 == 0);
+        }
+return even;
+    } else {
+        console.log("wrong request");
+    }
+}
