d9bc5ad698311c8e078cac433aab51136cdde4f6
Nisha <rainishani400@gmail.com>
2024-10-21 15:49:43 +0545

    Add files via upload

---
+++ part/try&catch.js
@@ -0,0 +1,14 @@
+console.log("hello");
+console.log("hey");
+let a=90;
+
+try{
+    console.log(a);
+    console.log(b);
+}catch(err){
+    console.log("caught an error  b is not define");
+    console.log(err);
+}
+console.log("are you ready");
+console.log("be careful");
+console.log("ok");
