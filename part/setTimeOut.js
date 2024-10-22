749ca18e110319356339fe49d9b7e34de663a06e
Nisha <rainishani400@gmail.com>
2024-10-22 07:06:05 +0545

    Update setTimeOut.js

--- part/setTimeOut.js
+++ part/setTimeOut.js
@@ -3,3 +3,15 @@ setTimeout(()=>{
     console.log(" nisha website.");
 },7000);
 console.log("welcome to.");
+//setInterval function run continues
+let id= setInterval(()=>{
+    console.log("nisha");
+},5000);
+console.log(id);
+let id1= setInterval(()=>{
+    console.log("nisha");
+},5000);
+console.log(id1);
+//for stop
+clearInterval(id);
+clearInterval(id1);
