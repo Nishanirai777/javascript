7893a04022b0bea161f51b85b14b3174662c5b42
Nisha <rainishani400@gmail.com>
2024-10-22 06:28:18 +0545

    Add files via upload

---
+++ part/arrowFunction.js
@@ -0,0 +1,13 @@
+const sum = (a,b)=>{console.log(a+b);
+
+};
+const cube =(n)=> {
+    return n* n*n;
+};
+//power function
+const pow =(a,b)=>{
+    return a**b;
+};
+const hello =()=>{
+    console.log("hello world");
+};
