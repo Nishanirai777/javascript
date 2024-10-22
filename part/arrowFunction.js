ec718d08e652d7b3271eff1ad1316878aa80ad9e
Nisha <rainishani400@gmail.com>
2024-10-22 06:36:31 +0545

    Update arrowFunction.js

--- part/arrowFunction.js
+++ part/arrowFunction.js
@@ -11,3 +11,7 @@ const pow =(a,b)=>{
 const hello =()=>{
     console.log("hello world");
 };
+//arrow fucntion formula
+//const  function name =(arg1,arg2)=>{value}
+//implicit return in arrow
+const mul= (a,b)=>(a*b);
