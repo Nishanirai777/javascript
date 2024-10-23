899cbaafb8d5ab0275b6f825e1d10abb6881e728
Nisha <rainishani400@gmail.com>
2024-10-23 07:53:53 +0545

    Update ps.js

--- part/ps.js
+++ part/ps.js
@@ -12,3 +12,16 @@
 setTimeout(()=>{
     clearInterval(id);
 },10000);
+//write an arrow function named arrayAverage that accepts an array of numbers and returns the average of those numbers.
+const arrayAveragee =(arr)=>{
+    let total =0;
+    for (let number of arr){
+        total +=number;
+    }
+    return total / arr.length;
+};
+let arr =[1,2,3,4,5,6];
+console.log(arrayAveragee(arr));
+//write an arrow function named is even ()that takes a single numbers as arguments and returns if it is even or not.
+let num =4;
+const isEven =(num)=>num%2 ==0;
