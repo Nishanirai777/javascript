7cf5a70f4c92b354c1a157b1993307426832be79
Nisha <rainishani400@gmail.com>
2024-10-23 09:18:43 +0545

    Add files via upload

---
+++ part8/map&filter.js
@@ -0,0 +1,30 @@
+let num = [1,2,3,4];
+let double = num.map((el)=>{
+    return el*2;
+});
+let student =[
+    {
+        name:"saraj",
+        marks: 100,
+    },
+    {
+        name:"raju",
+        marks: 50,
+    },
+    {
+        name:"saraj",
+        marks: 100,
+    },
+];
+let gpa = student.map((el)=>{
+    return el.marks/10
+});
+//filter
+let nums =[1,2,3,4,5,6,8,9,7];
+let ans =nums.filter((el)=>{
+    return el    % 2 ==0;
+    //for less than 5
+    return    el <5;
+    //for odd
+    //return el % 2!==0;
+});
