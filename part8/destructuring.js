55e6155106253c8df5bf841c9a361dd7d3fac349
Nisha <rainishani400@gmail.com>
2024-10-24 16:45:17 +0545

    Update destructuring.js

--- part8/destructuring.js
+++ part8/destructuring.js
@@ -4,3 +4,14 @@ let names=["nisha","saraj","peter","mithu"];
 //let c=names[3];
 //for short method
 let [a,b,c ,...pthers]=names;
+//destructuring for object
+const students={
+    name: "nisha",
+    age: 21,
+    class:16,
+    username:"@nisha123",
+    password:"vlkd",
+};
+//let username= students.username;
+//short method
+let {username,password ,city="sindhuli"}=students;
