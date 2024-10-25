35d51f24427a0385b7aa90227980864dd142593f
Nisha <rainishani400@gmail.com>
2024-10-25 10:02:20 +0545

    Update app.js

---
+++ part9/app.js
@@ -0,0 +1,6 @@
+let smallImages= document.getElementsByClassName("oldImg");
+for(let i=0;i<smallImages.length;i++){
+    console.dir(smallImages[i]);
+    //if you need a src then write this
+    console.dir(smallImages[i].src);
+}
