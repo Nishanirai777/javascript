194e019f180a39b58cc6f6288cd32fad6807501e
Nisha <rainishani400@gmail.com>
2024-10-26 19:09:27 +0545

    Add files via upload

---
+++ part9/ps.js
@@ -0,0 +1,9 @@
+let para1= document.createElement("p");
+para1.innerText="hey i'm red";
+document.querySelector("body").appendChild(para1);
+para1.classList.add("red");
+
+let h3= document.createElement("h3");
+h3.innerText="hey i'm blue";
+document.querySelector("body").appendChild(blue);
+h3.classList.add("blue");
