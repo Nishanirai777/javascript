666f95ed7dd0d5c2343dca5fb30db1e7d6aa3e52
Nisha <rainishani400@gmail.com>
2024-10-31 21:12:54 +0545

    Add files via upload

---
+++ part11/jsissinglr.js
@@ -0,0 +1,13 @@
+let a=6;
+console.log(a);
+let b=4;
+console.log(b);
+console.log(a+b);
+//this is synchrous nature
+setTimeout(()=> {
+    console.log("apana college");
+},2000);
+setTimeout(()=> {
+    console.log("hello world");
+},2000);
+console.log("hello...");
