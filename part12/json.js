88b4886df048ee8a2966e72e5f05bfeb8a7512e8
Nisha <rainishani400@gmail.com>
2024-11-18 16:39:57 +0545

    Add files via upload

---
+++ part12/json.js
@@ -0,0 +1,12 @@
+let jsonRes= 
+'{
+    "fact": "The cat's tail is used to maintain balance.",
+    "length": 43
+}';
+let validRes= JSON.parse(jsonRes);
+console.log(validRes.fact);
+let student ={
+    name : "nisha",
+    class : "bachelor",
+
+};
